1c92ecb1288ada5ec6907f57473f82d97da868b5
HappyWdz <634212523@qq.com>
2017-03-11 16:38:58 +0800

    3.Send Async Request

--- meizigirl/src/main/java/com/example/meizigirl/MainActivity.java
+++ meizigirl/src/main/java/com/example/meizigirl/MainActivity.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -26,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         // mListView = (ListView) findViewById(R.id.list_view);
         ButterKnife.bind(this); //绑定Activity
         sendSyncRequest();
+        sendAsyncRequest();
+    }
+
+    private void sendAsyncRequest() {
+        OkHttpClient okHttpClient =new OkHttpClient();
+        //创建一个网络请求
+        String url ="http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1";
+        Request request =new Request.Builder().get().url(url).build();
+       okHttpClient.newCall(request).enqueue(new Callback() {
+           //异步请求 ,不需要等到网络结果返回,就执行后面的代码,okhttply内部会在子线程执行网络请求,返回结果
+           @Override
+           public void onFailure(Call call, IOException e) {
+
+           }
+
+           @Override
+           public void onResponse(Call call, Response response) throws IOException {
+               String result = response.body().string();
+               Log.d(TAG, "sendAsyncRequest: "+result);
+           }
+       });
+
     }
 
     private void sendSyncRequest() {
