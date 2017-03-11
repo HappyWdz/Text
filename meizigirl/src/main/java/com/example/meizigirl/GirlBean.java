e08434a7491058226a65dbf714ea313b0566a66b
HappyWdz <634212523@qq.com>
2017-03-11 17:59:15 +0800

    4.刷新列表

---
+++ meizigirl/src/main/java/com/example/meizigirl/GirlBean.java
@@ -0,0 +1,18 @@
+package com.example.meizigirl;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class GirlBean {
+    public boolean error;
+
+    public List<GirlItem> resulte;
+
+    public class GirlItem{
+        public String _id;
+        public String created;
+    }
+}
--- meizigirl/src/main/java/com/example/meizigirl/MainActivity.java
+++ meizigirl/src/main/java/com/example/meizigirl/MainActivity.java
@@ -3,9 +3,18 @@ package com.example.meizigirl;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,12 +30,18 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.list_view)
     ListView mListView;
 
+    //数据集合
+    private List<ResultBean.ResultsBean> mListData =new ArrayList<ResultBean.ResultsBean>();
+
+    private Gson mGson =new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // mListView = (ListView) findViewById(R.id.list_view);
         ButterKnife.bind(this); //绑定Activity
+        mListView.setAdapter(mBaseAdapter);
         //sendSyncRequest();
         sendAsyncRequest();
     }
@@ -43,10 +58,27 @@ public class MainActivity extends AppCompatActivity {
 
            }
 
+           /**
+            *
+            * 在子线程被调用
+            */
            @Override
            public void onResponse(Call call, Response response) throws IOException {
                String result = response.body().string();
-               Log.d(TAG, "onResponse: "+result);
+               ResultBean resultBean = mGson.fromJson(result, ResultBean.class);
+               //Log.d(TAG, "onResponse: "+resultBean.getResults().get(0).getUrl());
+
+               //将网络结果加入数据集合
+               mListData.addAll(resultBean.getResults());
+               //在主线程刷新
+               runOnUiThread(new Runnable() {
+                   @Override
+                   public void run() {
+                       //通知Adapter刷新列表
+                       mBaseAdapter.notifyDataSetChanged();
+                   }
+               });
+
            }
        });
         Log.d(TAG, "sendAsyncRequest: ");
@@ -72,5 +104,50 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+
+    private BaseAdapter mBaseAdapter =new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            //当没有可回收利用的item
+            if (convertView ==null){
+                convertView =View.inflate(MainActivity.this,R.layout.view_list_item,null);
+                viewHolder =new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            //绑定视图
+            ResultBean.ResultsBean resultsBean = mListData.get(position);//拿到对应位置的数据
+            //更新发布时间
+            viewHolder.mTextView.setText(resultsBean.getPublishedAt());
+            return convertView;
+        }
+    };
+
+    public class  ViewHolder{
+        ImageView mImageView;
+        TextView mTextView;
+
+        public ViewHolder(View root){
+            mImageView = (ImageView) root.findViewById(R.id.image);
+            mTextView = (TextView) root.findViewById(R.id.publish_time);
+        }
     }
 }
